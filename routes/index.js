1abbb67330cb8b5ddc5e56237fcca1998bfe4cca
Al Farisi <codelife.alf@gmail.com>
2023-12-20 12:23:03 +0700

    edit base path

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function(_, res) {
+  res.status(200)
+    .json({
+      'error': false,
+      'message': 'Service Running',
+    })
+    .end();
 });
 
 module.exports = router;
